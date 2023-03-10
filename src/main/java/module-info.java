a9ba673a06c613931310e2570c6ac0418face84d
Stian Lyng <stianlyng@protonmail.com>
2023-03-10 20:07:08 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module no.ntnu.idatx2001.oblig4 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens no.ntnu.idatx2001.oblig4 to javafx.fxml;
+    exports no.ntnu.idatx2001.oblig4;
+}
